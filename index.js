7fd366792f27fed81ae9e316600e7cc7eba78717
popkorn96 <mpaloma2014@gmail.com>
2020-11-07 16:10:36 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+const random = Math.floor( Math.random() * 1000 ) + 1;
+const mod = 16 % 6;
+const max = Math.max(1, 5, 7, 13, 16, 17, 20);
